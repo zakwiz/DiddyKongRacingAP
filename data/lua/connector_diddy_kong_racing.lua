97d84293c7656029b0b3c517f86c4d8787e1e775
zakwiz <zakwiz@hotmail.com>
2024-07-30 10:48:44 -0400

    Update versions

--- data/lua/connector_diddy_kong_racing.lua
+++ data/lua/connector_diddy_kong_racing.lua
@@ -8,8 +8,8 @@ require('common')
 local socket = require("socket")
 local json = require('json')
 
-local SCRIPT_VERSION = 7
-local DKR_VERSION = "v0.2.5"
+local SCRIPT_VERSION = 8
+local DKR_VERSION = "v0.2.6"
 
 local player
 local seed
