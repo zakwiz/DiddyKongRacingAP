27bae959ad37fcfc7b8dc2435f999bb87c8b3f68
zakwiz <zakwiz@hotmail.com>
2025-02-02 14:54:18 -0500

    Update versions

--- worlds/diddy_kong_racing/__init__.py
+++ worlds/diddy_kong_racing/__init__.py
@@ -36,7 +36,7 @@ class DiddyKongRacingWorld(World):
     """Diddy Kong Racing is a kart racing game with a story mode, complete with bosses and hidden collectibles."""
 
     game = "Diddy Kong Racing"
-    apworld_version = "DKRv0.5.0"
+    apworld_version = "DKRv0.5.1"
     web = DiddyKongRacingWeb()
     topology_preset = True
     item_name_to_id = {}
